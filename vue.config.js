3e649bdba7e9df725fe5db22c1d9a533b44be63a
jeff <yinjiefeng@91ruler.com>
2019-05-20 17:36:57 +0800

    修改代理配置地址

--- vue.config.js
+++ vue.config.js
@@ -10,11 +10,11 @@ module.exports = {
   devServer: {
     proxy: {
         // proxy all requests starting with /api to jsonplaceholder
-        '/admin': {
-            target: 'http://47.99.200.198/admin',   //代理接口
+        '/sys': {
+            target: 'http://47.103.55.170/sys',   //代理接口
                 changeOrigin: true,
                 pathRewrite: {
-                '^/admin': ''    //代理的路径
+                '^/sys': ''    //代理的路径
             }
         }
     }
