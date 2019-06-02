0763f5346ea8d9f74e10a3b1c49ef106012b9a08
jeff <jeffreestyle@163.com>
2019-06-02 21:40:13 +0800

    favicon

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const development = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  publicPath: development ? '' : '/pc',
+  // publicPath: development ? '' : '',
   configureWebpack: config => {
     if (development) {
       config.devtool = 'source-map'
