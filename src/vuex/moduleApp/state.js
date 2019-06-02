91964b60704947b796d5c57c7fcee6f66da942de
JoeyXiao <joey_xiao_cn@icloud.com>
2019-06-02 22:29:49 +0800

    调整菜单顺序

--- src/vuex/moduleApp/state.js
+++ src/vuex/moduleApp/state.js
@@ -9,34 +9,6 @@ const state = {
         icon: 'el-icon-d-arrow-right',
         title: '首页'
     },{
-        name: 'SysUserManagement',
-        icon: 'el-icon-tickets',
-        title: '系统管理',
-        role: 'sys_admin',
-        subItems: [{
-            name: 'SysRoleList',
-            title: '系统角色'
-        },
-        {
-            name: 'SysUserList',
-            title: '系统用户'
-        }]
-    },{
-        name: 'BusinessManagement',
-        icon: 'el-icon-tickets',
-        title: '业务管理',
-        role: 'sys_admin',
-        subItems: [{
-          name: 'FundList',
-          title: '资金方'
-        },{
-          name: 'ProjectList',
-          title: '项目方'
-        },{
-          name: 'SuccessCaseList',
-          title: '成功案例'
-        }]
-    },{
       name: 'ClientManagement',
       icon: 'el-icon-tickets',
       title: '客户管理',
@@ -48,6 +20,34 @@ const state = {
         name: 'AppointmentProjectList',
         title: '投递项目列表'
       }]
+    },{
+      name: 'BusinessManagement',
+      icon: 'el-icon-tickets',
+      title: '业务管理',
+      role: 'sys_admin',
+      subItems: [{
+        name: 'FundList',
+        title: '资金方'
+      },{
+        name: 'ProjectList',
+        title: '项目方'
+      },{
+        name: 'SuccessCaseList',
+        title: '成功案例'
+      }]
+    },{
+      name: 'SysUserManagement',
+      icon: 'el-icon-tickets',
+      title: '系统管理',
+      role: 'sys_admin',
+      subItems: [{
+        name: 'SysRoleList',
+        title: '系统角色'
+      },
+        {
+          name: 'SysUserList',
+          title: '系统用户'
+        }]
     }
     ],
     tabs: [],
